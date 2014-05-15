41fb1d5909b1b3d5018954534583617647210bf3
Michael Merickel <michael@merickel.org>
2014-05-15 13:57:37 -0500

    added cmake

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get -y -q upgrade
-apt-get -y -q install curl build-essential git mercurial golang protobuf-compiler
+apt-get -y -q install curl cmake build-essential git mercurial golang protobuf-compiler
 
 # for geoip
 apt-get -y -q install geoip-bin libgeoip-dev
